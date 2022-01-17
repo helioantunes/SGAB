e75550321e60d6b24f17f09bcaf645ec07a52ba3
EnzoBraz <enzodesouzabraz2004@gmail.com>
2022-01-17 19:20:17 -0300

    Criação Classe Aquisicao

---
+++ sgab-core/src/sgab/model/dto/Aquisicao.java
@@ -0,0 +1,65 @@
+package sgab.model.dto;
+
+import sgab.model.dto.util.AquisicaoStatus;
+import sgab.model.dto.Pessoa;
+import sgab.model.dto.Fornecedor;
+import sgab.model.dto.Obra;
+
+
+public class Aquisicao {
+    private Pessoa pessoa;
+    private Long quantidade;
+    private Fornecedor fornecedor;
+    private AquisicaoStatus status;
+    private Obra obra;
+
+    //construtor 
+    public Aquisicao(Pessoa pessoa, Long quantidade, Fornecedor fornecedor, AquisicaoStatus status, Obra obra){
+        this.pessoa = pessoa;
+        this.quantidade = quantidade;
+        this.fornecedor = fornecedor;
+        this.status = status;
+        this.obra = obra;
+    }
+
+    //get e set pessoa
+    public Pessoa getPessoa() {
+        return pessoa;
+    }
+
+    public void setPessoa(Pessoa pessoa) {
+        this.pessoa = pessoa;
+    }
+    //get e set quantidade
+    public Long getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Long quantidade) {
+        this.quantidade = quantidade;
+    }
+    //get e set fornecedor
+    public Fornecedor getFornecedor() {
+        return fornecedor;
+    }
+
+    public void setFornecedor(Fornecedor fornecedor) {
+        this.fornecedor = fornecedor;
+    }
+    //get e set status
+    public AquisicaoStatus getStatus() {
+        return status;
+    }
+
+    public void setSatus(AquisicaoStatus status) {
+        this.status = status;
+    }
+    //get e set obra
+    public Obra getObra() {
+        return obra;
+    }
+
+    public void setObra(Obra obra) {
+        this.obra = obra;
+    }
+}
---
+++ sgab-core/src/sgab/model/dto/util/AquisicaoStatus.java
@@ -0,0 +1,8 @@
+package sgab.model.dto.util;
+
+public enum AquisicaoStatus {
+   ATIVO,          // ainda está com o usuário
+   CANCELADO,     // cancelado pelo bibliotecario/apagar
+   FINALIZADA;    // aquisição foi finalizada
+}
+
