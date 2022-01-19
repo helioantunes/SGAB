86041cd4b757b31b377d9616a4ae261913b18917
EnzoBraz <enzodesouzabraz2004@gmail.com>
2022-01-19 16:31:36 -0300

    Set Status consertado e Controller iniciado

---
+++ sgab-view-web/src/java/sgab/controller/AquisicaoController.java
@@ -0,0 +1,86 @@
+package sgab.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import sgab.model.dto.Aquisicao;
+import sgab.model.service.GestaoAquisicaoService;
+import java.util.List;
+import sgab.model.exception.PersistenciaException;
+
+public class AquisicaoController {
+    public static String listarTodos(HttpServletRequest request) {
+        String jsp = "";
+        try {
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+            jsp = "";
+        }
+        return jsp;
+    }
+
+    public static String listarAtivo(HttpServletRequest request) {
+        String jsp = "";
+        try {
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+            jsp = "";
+        }
+        return jsp;
+    }
+
+    public static String listarPendente(HttpServletRequest request) {
+        String jsp = "";
+        try {
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+            jsp = "";
+        }
+        return jsp;
+    }
+
+    public static String gravarAtivo(HttpServletRequest request) {
+        String jsp = "";
+        try {
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+            jsp = "";
+        }
+        return jsp;
+    }
+
+    public static String gravarPendente(HttpServletRequest request) {
+        String jsp = "";
+        try {
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+            jsp = "";
+        }
+        return jsp;
+    }
+
+    public static String gravarFinalizado(HttpServletRequest request) {
+        String jsp = "";
+        try {
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+            jsp = "";
+        }
+        return jsp;
+    }
+
+    public static String excluir(HttpServletRequest request) {
+        String jsp = "";
+        try {
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+            jsp = "";
+        }
+        return jsp;
+    }    
+}
