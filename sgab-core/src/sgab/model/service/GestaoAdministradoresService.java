5ef07cbba48f66b4cda17ca2677d3ad2cede00a5
Helio Antunes <helioantunes805@gmail.com>
2022-01-22 14:10:57 -0300

    Nova versão Service

--- sgab-core/src/sgab/model/service/GestaoAdministradoresService.java
+++ sgab-core/src/sgab/model/service/GestaoAdministradoresService.java
@@ -3,7 +3,10 @@ package sgab.model.service;
 import java.util.List;
 import sgab.model.dao.PessoasDAO;
 import sgab.model.dto.Pessoa;
+import sgab.model.dto.util.PessoaTipo;
 import sgab.model.dto.util.PessoaHelper;
+import sgab.model.dto.util.AdministradoresHelper;
+import java.util.LinkedList;
 import sgab.model.exception.NegocioException;
 
 public class GestaoAdministradoresService {
@@ -13,7 +16,8 @@ public class GestaoAdministradoresService {
         pessoasDAO = PessoasDAO.getInstance();
     }
 
-    public Long cadastrar(Pessoa pessoa) {
+    public Long cadastrarAdministrador(Pessoa pessoa) {
+        pessoa.setTipo(PessoaTipo.ADMINISTRADOR);
         List<String> exMsgs = PessoaHelper.validarPessoa(pessoa, pessoasDAO);
         
         if (!exMsgs.isEmpty()){
@@ -24,36 +28,127 @@ public class GestaoAdministradoresService {
         return pessoa.getId();
     }
     
-    public List<Pessoa> pesquisarAtivos() {
-        return pessoasDAO.listarAtivos();
+    public Long cadastrarGestor(Pessoa pessoa) {
+        pessoa.setTipo(PessoaTipo.GESTOR);
+        List<String> exMsgs = PessoaHelper.validarPessoa(pessoa, pessoasDAO);
+        
+        if (!exMsgs.isEmpty()){
+            throw new NegocioException(exMsgs);
+        }
+            
+        pessoasDAO.inserir(pessoa);
+        return pessoa.getId();
     }
-
-    public Pessoa pesquisarPorId(Long id){
-        return pessoasDAO.pesquisar(id);
-    } 
     
-    public Pessoa pesquisarConta(String login, String senha){
+    public List<Pessoa> pesquisarAdministradoresAtivos() {
+        List<Pessoa> pessoas = pessoasDAO.listarAtivos();
+        List<Pessoa> administradores = new LinkedList<>();
+        for(int i=0;i<pessoas.size();i++){
+            Pessoa pessoa = pessoas.remove(i);
+            if(pessoa.getTipo()!=PessoaTipo.BIBLIOTECARIO){
+                administradores.add(pessoa);
+            }
+        }
+        return administradores;
+    }
+    
+    public Pessoa pesquisarAdministradoresPorId(Long id){
+        Pessoa pessoa = pessoasDAO.pesquisar(id);
+        if(pessoa.getTipo()!=PessoaTipo.ADMINISTRADOR){
+            return pessoa;
+        }
+        else{
+            return null;
+        }
+    }
+    
+    public Pessoa pesquisarAdministradoresPorConta(String login, String senha){
         
         Pessoa result = pessoasDAO.pesquisarLoginSenha(login, senha);
-        return result;               
+        if(result.getTipo()!=PessoaTipo.ADMINISTRADOR){
+            return result;
+        }
+        else{
+            return null;
+        }             
     }
 
-    public Pessoa pesquisarPorLogin(String login){
+    public Pessoa pesquisarAdministradoresPorLogin(String login){
         Pessoa result = pessoasDAO.pesquisarLogin(login);
-        return result;
+        if(result.getTipo()!=PessoaTipo.ADMINISTRADOR){
+            return result;
+        }
+        else{
+            return null;
+        } 
+    }
+    
+    public List<Pessoa> pesquisarGestorAtivos() {
+        List<Pessoa> pessoas = pessoasDAO.listarAtivos();
+        List<Pessoa> atendentes = new LinkedList<>();
+        for(int i=0; i<pessoas.size(); i++){
+            Pessoa pessoa = pessoas.remove(i);
+            if(pessoa.getTipo()!=PessoaTipo.GESTOR){
+                atendentes.add(pessoa);
+            }
+        }
+        return atendentes;
     }
 
-    public void excluir(Pessoa pessoa){
-        pessoasDAO.delete(pessoa.getId());    
+    public Pessoa pesquisarGestorPorId(Long id){
+        Pessoa pessoa = pessoasDAO.pesquisar(id);
+        if(pessoa.getTipo()!=PessoaTipo.GESTOR){
+            return pessoa;
+        }
+        else{
+            return null;
+        }
     }   
-    
-    public void alterar(Pessoa pessoa){
-        List<String> exMsgs = PessoaHelper.validarAlteracao(pessoa, pessoasDAO);
+
+    public Pessoa pesquisarGestorConta(String login, String senha){
+        
+        Pessoa result = pessoasDAO.pesquisarLoginSenha(login, senha);
+        if(result.getTipo()!=PessoaTipo.GESTOR){
+            return result;
+        }
+        else{
+            return null;
+        }                   
+    }
+
+    public Pessoa pesquisarGestorPorLogin(String login){
+        Pessoa result = pessoasDAO.pesquisarLogin(login);
+        if(result.getTipo()!=PessoaTipo.GESTOR){
+            return result;
+        }
+        else{
+            return null;
+        } 
+    }
+
+    public void alterarGestor(Pessoa pessoa){
+        List<String> exMsgs = AdministradorHelper.validarGestorAlteracao(pessoa, pessoasDAO);
         
         if (!exMsgs.isEmpty())
             throw new NegocioException(exMsgs);
         
         pessoasDAO.alterar(pessoa);
     }
+    
+    public void alterarAdministrador(Pessoa pessoa){
+        List<String> exMsgs =  AdministradoresHelper.validarAdministradorAlteracao(pessoa, pessoasDAO);
+        
+        if (!exMsgs.isEmpty())
+            throw new NegocioException(exMsgs);
+        
+        pessoasDAO.alterar(pessoa);
+    }
+
+    public void excluir(Pessoa pessoa){
+        pessoasDAO.delete(pessoa.getId());    
+    }   
+    
+   
+
 }
 
