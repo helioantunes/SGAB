cdca34baf6d825ac8199bd77bbb8fe2d17e70b64
EnzoBraz <enzodesouzabraz2004@gmail.com>
2022-01-31 11:05:38 -0300

    Reservas DAO e DTO

---
+++ sgab-core/src/sgab/model/dao/ReservaDAO.java
@@ -0,0 +1,91 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sgab.model.dao;
+import sgab.model.dto.Reserva;
+import sgab.model.exception.PersistenciaException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import sgab.model.dto.Biblioteca;
+import sgab.model.dto.Obra;
+/**
+ *
+ * @author T-Gamer
+ */
+public class ReservaDAO implements GenericDAO<Reserva, Long> {
+    private Map<Long, Reserva> table = new HashMap<>();
+    
+    private static ReservaDAO reservaDAO;
+    static {
+        ReservaDAO.reservaDAO = null;
+    }
+    
+    private static Long idSequence;
+    static {
+        ReservaDAO.idSequence = 0L;
+    }
+    
+    public static Long getNextId() {
+        return ReservaDAO.idSequence++;
+    } 
+    
+    private ReservaDAO() { }
+    
+    @Override
+    public void inserir(Reserva entidade) {
+        if (pesquisar(entidade.getId()) != null)
+            throw new PersistenciaException("'" + entidade.getId() 
+                                                            + "' precisa ser único.");
+        
+        Long reservaId = ReservaDAO.getNextId();
+        entidade.setId(reservaId);
+        
+        
+        table.put(reservaId, entidade);
+    }
+
+    @Override
+    public void alterar(Reserva entidade) {
+        Reserva reserva = table.remove(entidade.getId());
+        if (reserva == null)
+            throw new PersistenciaException("Nenhuma reserva com " + "o id '" + entidade.getId() + "'.");
+        
+        inserir(entidade);
+    }
+
+    @Override
+    public Reserva pesquisar(Long id) {
+         return table.get(id);
+    }
+
+    @Override
+    public List<Reserva> pesquisar(List<BasicPair<Long, Reserva>> parameterList) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    public List<Reserva> listarTodos() {
+        List<Reserva> listReservas = new ArrayList<>();
+        
+        listReservas.addAll(table.values());
+        
+        return listReservas;
+    }
+    
+    public List<Reserva> listarPorObraBiblioteca(Obra obra, Biblioteca biblioteca) {
+        List<Reserva> listReservas = new ArrayList<>();
+        
+        for(Reserva reserva : table.values()) {
+            if(reserva.getObra() == obra && reserva.getLocalReserva() == biblioteca)
+                listReservas.add(reserva);
+        }
+        
+        return listReservas;
+    }
+    
+    public void delete(Long id){
+        table.remove(id);
+    }
+}
---
+++ sgab-core/src/sgab/model/dto/Reserva.java
@@ -0,0 +1,87 @@
+package sgab.model.dto;
+
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+import sgab.model.dto.Exemplar;
+import java.util.ArrayList;
+
+public class Reserva {
+    private Long id;
+    private Pessoa pessoa;
+    private Obra obra;
+    private Biblioteca localReserva; 
+    private ArrayList<Exemplar> exemplares;
+    private final String horario;
+    
+    
+    //construtor 
+    public Reserva(Pessoa pessoa, Obra obra, Biblioteca localReserva, ArrayList<Exemplar> exemplares, String horario){
+        this.pessoa = pessoa;
+        this.obra = obra;
+        this.localReserva = localReserva;
+        this.exemplares =  new ArrayList<>(exemplares);
+
+        Date date = new Date();
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy - HH:mm:ss");
+        
+        this.horario = dateFormat.format(date);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Pessoa getPessoa() {
+        return pessoa;
+    }
+    public void setPessoa(Pessoa pessoa) {
+        this.pessoa = pessoa;
+    }
+
+    public Obra getObra() {
+        return obra;
+    }
+    public void setObra(Obra obra) {
+        this.obra = obra;
+    }
+
+    public Biblioteca getLocalReserva() {
+        return localReserva;
+    }
+    public void setLocalReserva(Biblioteca localReserva) {
+        this.localReserva = localReserva;
+    }
+
+    public ArrayList getExemplares() {
+        return this.exemplares;
+    }
+    public void setExemplar(Exemplar exemplar) {
+        if (!exemplares.contains(exemplar))
+            this.exemplares.add(exemplar);
+    }
+
+    public void setExemplares(ArrayList<Exemplar> exemplares) {
+        this.exemplares =  new ArrayList<>(exemplares);
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    
+    
+}
