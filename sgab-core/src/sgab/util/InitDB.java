0649b430360ec7cf070bed3f9c657d547e6b4ace
EnzoBraz <enzodesouzabraz2004@gmail.com>
2022-02-01 23:18:58 -0300

    InitDB com exemplar

--- sgab-core/src/sgab/util/InitDB.java
+++ sgab-core/src/sgab/util/InitDB.java
@@ -159,6 +159,19 @@ public class InitDB {
                 "Av. dos Imigrantes, 1000 - Varginha/MG"));
     }
     
+    
+    private static void cadastrarExemplar() {
+        GestaoObras manterObra = new GestaoObras();
+        GestaoAcervo manterAcervo = new GestaoAcervo();
+        GestaoBibliotecaService manterBiblioteca = new GestaoBibliotecaService();
+
+        Exemplar exemplar = new Exemplar(manterObra.pesquisarObraNome("Algoritmos - Teoria e Prática").get(0), 
+                manterBiblioteca.pesquisarProNome("Biblioteca Central"));
+        exemplar.setId((long) 1);
+        exemplar.setStatus(ExemplarStatus.DISPONIVEL);
+        manterAcervo.cadastrarExemplar(exemplar);
+    }
+    
     public static void init() {
         if (initAccess)
             return;
@@ -170,6 +183,7 @@ public class InitDB {
         InitDB.cadastrarFornecedor();
         InitDB.cadastrarObra();
         InitDB.cadastrarPessoa();
+        InitDB.cadastrarExemplar();
         
         initAccess = true;
     }
