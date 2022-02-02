8ee34eff4882f6f2d7141ab25a3b9caa29726163
cmaffort <maffort@gmail.com>
2022-02-02 07:51:22 -0300

    Correção de bug após integração de pull #63

--- sgab-core/src/sgab/model/dto/Exemplar.java
+++ sgab-core/src/sgab/model/dto/Exemplar.java
@@ -10,6 +10,7 @@ public class Exemplar {
     private ExemplarStatus status;
     private ExemplarTipo tipo;
     private Biblioteca bibliotecaPosse;
+    private String historico;    
 
     public Exemplar(Obra obra, Biblioteca biblioteca) {
         this.obra = obra;
@@ -54,6 +55,12 @@ public class Exemplar {
         this.bibliotecaPosse = bibliotecaPosse;
     }
     
+    public String getHistorico() {
+        return historico;
+    }
 
+    public void setHistorico(String historico) {
+        this.historico = historico;
+    }
 }
 
